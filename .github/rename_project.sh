61aa4237417552a99c58459b87f189f2767724a8
inkiyad <inkiyad@users.noreply.github.com>
2023-12-21 19:56:37 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="inkiyad"
+original_name="property_analysis"
+original_urlname="property_analysis"
+original_description="Awesome property_analysis created by inkiyad"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv property_analysis $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
